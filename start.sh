c048829d869b85bb9cc9f749bd827491fd297fda
spidyynaik <128451864+spidyynaik@users.noreply.github.com>
2023-04-02 16:07:06 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/spidyynaik/AutoFilter
+  git clone https://github.com/spidyynaik/AutoFilter /Auto-Filter-V5
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Auto-Filter-V5
